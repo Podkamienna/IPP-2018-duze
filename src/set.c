f16b489f3c3b76b8aa5ef7ddf93e89909a39a1ca
Alicja <ala.ziarko@gmail.com>
2019-07-05 23:02:30 +0200

    added set

---
+++ src/set.c
@@ -0,0 +1,12 @@
+/** @file
+ * @author Antoni Żewierżejew <azewierzejew@gmail.com>
+ * @date 05.07.2019
+ */
+
+#include "set.h"
+
+Set *initializeSet();
+
+void *searchSet(bool isOk(void *));
+
+void freeSet();
---
+++ src/set.h
@@ -0,0 +1,19 @@
+/** @file
+ * @author Antoni Żewierżejew <azewierzejew@gmail.com>
+ * @date 05.07.2019
+ */
+
+#ifndef DROGI_SET_H
+#define DROGI_SET_H
+
+#include <stdbool.h>
+
+typedef struct Set Set;
+
+Set *initializeSet();
+
+void *searchSet(bool isOk(void *));
+
+void freeSet();
+
+#endif /* DROGI_SET_H */
